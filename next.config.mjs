b7adab9f7afa1c70e88527c0c68bb4ee0c9e271a
phillyfroyo <philipwooleryprice@gmail.com>
2025-06-28 13:32:56 -0500

    Add Webpack alias to next.config.mjs for @ path resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
