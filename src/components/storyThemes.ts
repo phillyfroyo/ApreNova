218ac60fb086ba2d60a5cce2070d51d4e6511aab
phillyfroyo <philipwooleryprice@gmail.com>
2025-07-06 08:53:23 -0500

    modified src/components/storyThemes.ts to include a default theme

--- src/components/storyThemes.ts
+++ src/components/storyThemes.ts
@@ -1,5 +1,12 @@
 // storyThemes.ts
 export const STORY_THEMES = {
+  default: {
+    backgroundImage: "/images/background4.png",
+    textColor: "text-gray-900",
+    accentColor: "bg-green-600",
+    hoverAccentColor: "hover:bg-green-300",
+    fontFamily: "font-sans",
+  },
     aventura: {
     backgroundImage: "/images/background4.png",
     textColor: "text-gray-900",
